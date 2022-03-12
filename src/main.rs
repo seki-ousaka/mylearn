203ed7250cb444752d49f4d4aa64088826c8ce39
seki <>
2022-03-12 17:36:00 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,19 @@ use learn::t_1::*;
  * my learning entry
  */
 fn main() {
+    // // Sum
+    // t_1_1::t_1_1_do();
+
+    // // mod abs pow sqrt and the four basic arithmetic operations
+    // t_1_2::t_1_2_do();
+
+    // // bit operation
+    // t_1_3::t_1_3_do();
+
+    t_1();
+}
+
+fn t_1(){
     // Sum
     t_1_1::t_1_1_do();
 
